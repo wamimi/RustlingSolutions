f0f19f10211d586bdbf33a92b14f37814e29c584
Nelly Njeri <nellycyberpro@gmail.com>
2024-06-03 13:29:14 +0300

    Executing Functions section! Less gooogit status!

---
+++ src/2_functions/functions1.rs
@@ -0,0 +1,18 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+// I AM NOT DONE
+
+fn main() {
+    call_me();
+}
+fn call_me(){
+
+}
+// In this exercise, I learned the basics of defining and calling functions in Rust:
+
+// Defining Functions: The fn keyword is used to define a function. Here, I defined a simple function named call_me.
+// Calling Functions: Functions are called by their name followed by parentheses. The main function calls call_me.
+// Main Function: Every Rust program has a main function, which serves as the entry point. 
