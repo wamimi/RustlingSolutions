c40d369e92abfa0abb008b01b0df0932258eab0c
Nelly Njeri <nellycyberpro@gmail.com>
2024-06-12 13:06:18 +0300

    Add solution for primitive_types4.rs

---
+++ src/4_primitive_types/primitive_types4.rs
@@ -0,0 +1,20 @@
+// primitive_types4.rs
+//
+// Get a slice out of Array a where the ??? is so that the test passes.
+//
+// Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
+// for a hint.
+// In this exercise, I learned about:
+
+// Array Slices: How to create a slice from an array in Rust.
+
+
+
+#[test]
+fn slice_out_of_array() {
+    let a = [1, 2, 3, 4, 5];
+
+    let nice_slice = [2,3,4];
+
+    assert_eq!([2, 3, 4], nice_slice)
+}
