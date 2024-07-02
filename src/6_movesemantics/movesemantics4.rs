e4642d3c15f55cd78c9e1aa19e134629bcbf01cc
Nelly Njeri <nellycyberpro@gmail.com>
2024-07-02 12:03:42 +0300

    Added solution for movesemantics4.rs

---
+++ src/6_movesemantics/movesemantics4.rs
@@ -0,0 +1,47 @@
+// move_semantics4.rs
+//
+// Refactor this code so that instead of passing `vec0` into the `fill_vec`
+// function, the Vector gets created in the function itself and passed back to
+// the main function.
+//
+// Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand
+// for a hint.
+
+// I AM NOT DONE
+
+#[test]
+fn main() {
+
+    let vec1 = fill_vec();
+
+    assert_eq!(vec1, vec![22, 44, 66, 88]);
+}
+
+// `fill_vec()` no longer takes `vec: Vec<i32>` as argument - don't change this!
+fn fill_vec() -> Vec<i32> {
+    // Instead, let's create and fill the Vec in here - how do you do that?
+    let mut vec = vec![22, 44, 66];
+
+    vec.push(88);
+
+    vec
+}
+/*
+Key Concepts Learned:
+Function Responsibility: This exercise emphasized moving the responsibility of creating and initializing data structures into the function itself, rather than passing them as parameters.
+This can simplify the calling code and encapsulate the logic within the function.
+
+Function Signatures: Understanding how to modify function signatures and adapt the calling code accordingly is essential. 
+Here, fill_vec was changed to take no parameters and return a newly created vector.
+
+Initialization within Functions: I learned how to initialize and manipulate data structures directly within functions. 
+This included:
+
+Creating a new vector.
+Initializing it with specific values.
+Modifying it before returning.
+Ownership and Returning Values: The exercise also reinforced the concept of ownership transfer in Rust. 
+By creating and modifying the vector within fill_vec and returning it, we transfer the ownership of the vector to the caller (main function in this case).
+
+Avoiding Unnecessary Parameters: By creating the vector inside the function, we avoided passing unnecessary parameters, making the function more self-contained and reducing dependencies.
+*/
